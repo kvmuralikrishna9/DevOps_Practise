dc86dfbcc8cd3f6ff4f52651c7cd8bc7baa443f6
kvmuralikrishn9 <kvmuralikrishn9@outlook.com>
2024-07-29 12:05:18 +0530

    practise Session Shell Sh

---
+++ ShellScript/1_helloworld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "hello world, this is the conversaton between Himanish and Swarna.
+
+Swarna: Hello Himanish,
+Hianish: Hello Swarna
+Swarna: How r u himanish
+Himanish: Fine mom"
