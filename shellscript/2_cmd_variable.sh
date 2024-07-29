a42965ff7fefba402cedca526c2413011f07aa39
kvmuralikrishn9 <kvmuralikrishn9@outlook.com>
2024-07-29 12:56:48 +0530

    learning

--- shellscript/2_cmd_variable.sh
+++ shellscript/2_cmd_variable.sh
@@ -10,6 +10,6 @@ echo "This script was executed on $DATE,
 Hello world, this is the conversaton between $PERSON2 and $PERSON1.
 
 $PERSON1: Hello $PERSON2,
-$PERSON2: Hello $PERSON1
-$PERSON1: How r u $PERSON2
+$PERSON2: Hello $PERSON1,
+$PERSON1: How r u $PERSON2,
 $PERSON2: Fine mom."
