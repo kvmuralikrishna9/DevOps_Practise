49ffba5d052e4872c63dc30ba46202e82d7c8314
murali <murali@gmail.com>
2024-07-29 12:55:38 +0530

    hello

--- shellscript/2_cmd_variable.sh
+++ shellscript/2_cmd_variable.sh
@@ -5,11 +5,11 @@ PERSON1=Swarna
 PERSON2=Himanish
 DATE=$(date)
 
-echo This script was executed on $DATE, 
+echo "This script was executed on $DATE, 
 
 Hello world, this is the conversaton between $PERSON2 and $PERSON1.
 
-"$PERSON1: Hello $PERSON2,"
-"$PERSON2: Hello $PERSON1"
-"$PERSON1: How r u $PERSON2"
-"$PERSON2: Fine mom.
+$PERSON1: Hello $PERSON2,
+$PERSON2: Hello $PERSON1
+$PERSON1: How r u $PERSON2
+$PERSON2: Fine mom."
