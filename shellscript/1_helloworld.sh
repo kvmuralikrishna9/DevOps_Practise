04cd20da82dba3e047b55b75ec148a4e11a1482e
murali <murali@gmail.com>
2024-07-29 12:39:41 +0530

    hello

--- shellscript/1_helloworld.sh
+++ shellscript/1_helloworld.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PERSON1=Swarna
-PERSON2=$PERSON2
+PERSON2=Himanish
 
 echo "hello world, this is the conversaton between $PERSON2 and $PERSON1.
 
 '$PERSON1: Hello $PERSON2,'
-'Hianish: Hello $PERSON1'
+'$PERSON2: Hello $PERSON1'
 '$PERSON1: How r u $PERSON2'
 '$PERSON2: Fine mom.'"
