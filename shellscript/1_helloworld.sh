8796f3d680e834f75de3ef5a679671e1324e5b79
kvmuralikrishn9 <kvmuralikrishn9@outlook.com>
2024-07-29 12:42:45 +0530

    learning

--- shellscript/1_helloworld.sh
+++ shellscript/1_helloworld.sh
@@ -5,7 +5,7 @@ PERSON2=Himanish
 
 echo "hello world, this is the conversaton between $PERSON2 and $PERSON1.
 
-'$PERSON1: Hello $PERSON2,'
-'$PERSON2: Hello $PERSON1'
-'$PERSON1: How r u $PERSON2'
-'$PERSON2: Fine mom.'"
+"$PERSON1: Hello $PERSON2,"
+"$PERSON2: Hello $PERSON1"
+"$PERSON1: How r u $PERSON2"
+"$PERSON2: Fine mom.""
